b3cf912913a1c7c656bdb63a5d9e1da6d3389720
Ankit Chaudhary <ankitchaudhary7157@gmail.com>
2026-06-25 20:11:34 +0530

    File Storage: Added S3 Config

---
+++ src/config/s3.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv'
+import { S3Client } from "@aws-sdk/client-s3";
+
+dotenv.config()
+
+export const s3 = new S3Client({
+  region: process.env.SUPABASE_S3_REGION,
+  endpoint: process.env.SUPABASE_S3_ENDPOINT,
+  forcePathStyle: true,
+
+  credentials: {
+    accessKeyId: process.env.SUPABASE_S3_ACCESS_KEY,
+    secretAccessKey: process.env.SUPABASE_S3_SECRET_KEY,
+  },
+});
